65c0d539344f59ade7d1a08c84401ec257c22b02
Tsavlis Eleftherios <Tsavlhslef@gmail.com>
2025-03-07 03:37:04 -1000

    Test change in Hello World!

--- gr/aueb/cf/lef/ch1/HelloWorld1.java
+++ gr/aueb/cf/lef/ch1/HelloWorld1.java
@@ -4,7 +4,7 @@ package gr.aueb.cf.lef.ch1;
 public class HelloWorld1 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World");
         System.out.printf("Hello World!");
         System.out.printf("Hello World!");
 
